fc71900ae33b175440ae5c872a6af69e36fd31ae
Tomás Deluca <Tomasdelucaa@gmail.com>
2025-03-08 20:32:47 -0300

    Simplify Progress component by removing unnecessary className prop

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -6,7 +6,7 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className="relative h-2 w-full overflow-hidden rounded-full bg-purple-900/20"
