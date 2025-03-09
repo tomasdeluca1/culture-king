4de6cea277b6ce15f9fbcb39edea628269f3b4d5
Tomás Deluca <Tomasdelucaa@gmail.com>
2025-03-09 20:17:22 -0300

    Refactor leaderboards route with improved date range calculation
    - Extracted date range calculation logic into a separate `getStartDate` function
    - Simplified leaderboards route by moving date range logic out of the main function
    - Improved code readability and maintainability
    - Removed unnecessary error catching in the MongoDB query

--- src/app/api/leaderboards/route.ts
+++ src/app/api/leaderboards/route.ts
@@ -6,30 +6,17 @@ export const runtime = "nodejs";
 export const maxDuration = 10;
 
 export async function GET(request: Request) {
+  const period = new URL(request.url).searchParams.get("period") || "daily";
+
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGODB_DATABASE);
+  const collection = db.collection("daily_challenges");
+
+  // Calculate date ranges
+  const now = new Date();
+  const startDate = getStartDate(period, now);
+
   try {
-    const period = new URL(request.url).searchParams.get("period") || "daily";
-
-    const client = await clientPromise;
-    const db = client.db(process.env.MONGODB_DATABASE);
-    const collection = db.collection("daily_challenges");
-
-    // Calculate date ranges
-    const now = new Date();
-    let startDate = new Date();
-
-    switch (period) {
-      case "daily":
-        startDate.setHours(0, 0, 0, 0);
-        break;
-      case "monthly":
-        startDate = new Date(now.getFullYear(), now.getMonth(), 1);
-        break;
-      case "yearly":
-        startDate = new Date(now.getFullYear(), 0, 1);
-        break;
-      default:
-        startDate.setHours(0, 0, 0, 0);
-    }
     const topScores = await collection
       .find({ date: { $gte: startDate } })
       .sort({ score: -1, timeTaken: 1 })
@@ -44,11 +31,7 @@ export async function GET(request: Request) {
         timeTaken: 1,
         date: 1,
       })
-      .toArray()
-      .catch((error) => {
-        console.error("Error fetching top scores:", error);
-        throw new Error("Failed to fetch top scores");
-      });
+      .toArray();
 
     return NextResponse.json(topScores);
   } catch (error) {
@@ -58,4 +41,22 @@ export async function GET(request: Request) {
       { status: 500 }
     );
   }
+}
+
+function getStartDate(period: string, now: Date): Date {
+  const startDate = new Date();
+  switch (period) {
+    case "daily":
+      startDate.setHours(0, 0, 0, 0);
+      break;
+    case "monthly":
+      startDate.setFullYear(now.getFullYear(), now.getMonth(), 1);
+      break;
+    case "yearly":
+      startDate.setFullYear(now.getFullYear(), 0, 1);
+      break;
+    default:
+      startDate.setHours(0, 0, 0, 0);
+  }
+  return startDate;
 }
